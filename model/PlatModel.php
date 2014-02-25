5f35a12ff07ace38670ef002debe7e109e577247
lucasgehin <lucasgehin88@gmail.com>
2014-02-25 08:55:31 +0100

    squelette Modeles

---
+++ model/PlatModel.php
@@ -0,0 +1,24 @@
+<?php
+
+	class PlatModel {
+
+		public function __construct()
+		{
+			# code...
+		}
+
+		public function find($id)
+		{
+			# code...
+		}
+
+		public function findAll()
+		{
+			# code...
+		}
+
+		public function findRel($id, $relation)
+		{
+			# code...
+		}
+	}
---
+++ model/ThemeModel.php
@@ -0,0 +1,24 @@
+<?php
+
+	class ThemeModel {
+
+		public function __construct()
+		{
+			# code...
+		}
+
+		public function find($id)
+		{
+			# code...
+		}
+
+		public function findAll()
+		{
+			# code...
+		}
+
+		public function findRel($id, $relation)
+		{
+			# code...
+		}
+	}
